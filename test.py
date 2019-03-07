9fdc538b955e39a9301cdb0dc149a86f041354ff
Jason <jasonwest1013@yahoo.com>
2019-03-07 00:21:13 -0500

    added gradient descent algorithm

--- test.py
+++ test.py
@@ -3,6 +3,20 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+def gradientDescent(x, y, w, p):
+    iterations = 200
+    t = 0.1
+    m = len(y)
+
+    for i in range(iterations):
+        yHat = x.dot(w)
+        gradient = x.T.dot(yHat - y) / m
+        w = w - t * (gradient + (p/m) * w)
+
+    return w
+
+
 # Load the data from MatLab file and set as matrices
 mat = scipy.io.loadmat('data.mat')
 arrayX = np.array(mat['X'])
@@ -11,28 +25,26 @@ matrixX = np.matrix(mat['X'])
 matrixY = np.matrix(mat['Y'])
 lamb = 1
 
+# print(matrixX[:, [2]])
+# print(column)
+
 if (np.linalg.matrix_rank(matrixX) == min(np.size(matrixX, 1), np.size(matrixX, 0))):
     print("The matrix is full rank.\n")
 
 # find omega using the equation w = (X^T * X)^-1 * X^T * Y
 w = (matrixX.T * matrixX).I * matrixX.T * matrixY
 
-print("The MLE estimate for w is:")
+# print("The MLE estimate for w is:")
 print(w)
 
-print("\n")
+# print("\n")
 
 # solution using the closed form solution found in the slides
 w2 = ((matrixX.T * matrixX) + (lamb * np.identity(np.size(matrixX, 1)))
       ).I * matrixX.T * matrixY
 
-print("When Lambda = 1 and p = 2 w is: ")
-print(w2)
+# print("When Lambda = 1 and p = 2 w is: ")
+# print(w2)
 
-
-# plt.scatter(arrayY, arrayX[:, 0], arrayY, arrayX[:, 1])
-# plt.scatter(arrayY, arrayX[:, 0])
-# plt.show()
-
-# print(matrixX[:, [2]])
-# print(column)
+w0 = np.zeros((3, 1))
+print(gradientDescent(matrixX, matrixY, w0, 1))
