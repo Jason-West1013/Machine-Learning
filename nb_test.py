6352bbc58d224152e2c53daff938b294036db543
Jason <jasonwest1013@yahoo.com>
2019-03-12 00:07:53 -0400

    finished nb_train starting nb_test

---
+++ nb_test.py
@@ -0,0 +1,4 @@
+import scipy.io
+import numpy as np
+from nb_train import nb
+print(nb)
--- nb_train.py
+++ nb_train.py
@@ -5,25 +5,32 @@ mat = scipy.io.loadmat('breast-cancer-data.mat')
 
 X = np.matrix(mat['X'])
 Y = np.matrix(mat['Y'])
-P = np.size(X,1)
+P = np.size(X, 1)
 N = len(Y)
 
-sigma_x_arr = []
+nb = {}
 mu_x_given_y_arr = [[0 for x in range(1)] for y in range(P)]
 
+# probability of Y
 prob_y = (Y == 1).sum() / N
-print(prob_y)
+nb['prob_y'] = prob_y
 
 # compute the mean of X given Y
 for i in range(P):
     y_equals_0 = np.mean(X[:, [i]][np.where(Y == 0)])
     y_equals_1 = np.mean(X[:, [i]][np.where(Y == 1)])
     mu_x_given_y_arr[i] = [y_equals_0, y_equals_1]
-mu_x_given_y = np.matrix(mu_x_given_y_arr)
-print(mu_x_given_y)
 
-#the standard deviation for each feature
+mu_x_given_y = np.matrix(mu_x_given_y_arr)
+nb['mu_x_given_y'] = mu_x_given_y
+
+# compute the standard deviation based off the mean of X given Y
+new_X = np.matrix(np.empty([N, P]))
 for i in range(P):
-    sigma_x_arr = np.append(sigma_x_arr, np.std(X[:, [i]]))
-sigma_x = np.matrix(sigma_x_arr).T
-print(sigma_x)
+    new_X[:, i][np.where(Y == 0)] = np.subtract(
+        X[:, i][np.where(Y == 0)], mu_x_given_y[:, 0][i])
+    new_X[:, i][np.where(Y == 1)] = np.subtract(
+        X[:, i][np.where(Y == 1)], mu_x_given_y[:, 1][i])
+
+sigma_x = new_X.std(0).T
+nb['sigma_x'] = sigma_x
